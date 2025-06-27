1833e9175cafd3fdfc75967a868adaa6b9831e18
Danielito games <scalleyovera@gmail.com>
2025-06-27 00:59:05 -0500

    [FEAT] login and register and endpoint to test

--- api-ventas-admin/src/main/java/com/cibertec/model/Usuario.java
+++ api-ventas-admin/src/main/java/com/cibertec/model/Usuario.java
@@ -3,15 +3,20 @@ package com.cibertec.model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUsuario")
     private Integer idUsuario;
 
     @Column(length = 100)
@@ -31,4 +36,13 @@ public class Usuario {
 
     @Column(insertable = false, updatable = false)
     private LocalDateTime fechaRegistro;
+
+    public Usuario(String nombreCompleto, String correo, String clave, Rol rol, Boolean esActivo, LocalDateTime fechaRegistro) {
+        this.nombreCompleto = nombreCompleto;
+        this.correo = correo;
+        this.clave = clave;
+        this.rol = rol;
+        this.esActivo = esActivo;
+        this.fechaRegistro = fechaRegistro;
+    }
 }
