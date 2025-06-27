fddc3ca40c72f7d48a67997ee940aea0a82759e2
Danielito games <scalleyovera@gmail.com>
2025-06-27 00:58:27 -0500

    [FIX] jwt implement

---
+++ api-ventas-admin/src/main/java/com/cibertec/crosscutting/utils/JwtUtil.java
@@ -0,0 +1,74 @@
+package com.cibertec.crosscutting.utils;
+
+import com.cibertec.model.Rol;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public int getExpiration() {
+        return expiration;
+    }
+
+    public String getToken(Authentication authentication){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration * 1000L))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public Claims extractAllClaims(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+
+}
--- api-ventas-admin/src/main/java/com/cibertec/repository/RolRepository.java
+++ api-ventas-admin/src/main/java/com/cibertec/repository/RolRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cibertec.model.Rol;
 
+import java.util.Optional;
+
 public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    Optional<Rol> findByNombre(String nombre);
 }
