a8e1a53bbc48bf2c9ea85d6ba688a568146f1055
KeveloperCod <dannylatorrec@hotmail.com>
2025-06-30 15:29:07 -0500

    usuario modals funcionando

--- AppSistemaVentaAdmin/src/main.ts
+++ AppSistemaVentaAdmin/src/main.ts
@@ -1,21 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // 👈
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { SharedModule } from './app/Reutilizable/shared/shared.module';
 
-
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),              
+    provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    provideAnimations(),
-
-    importProvidersFrom(BrowserModule, SharedModule)
+    provideAnimations(), // Ya lo tienes, igual déjalo
+    importProvidersFrom(BrowserAnimationsModule), // 👈 Esta línea es la clave
+    importProvidersFrom(SharedModule)
   ]
 }).catch(err => console.error(err));
